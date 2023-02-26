450003f77b1563f625ae611a65ed8335860cef2b
Kazuaki Tokunaga <ktokunaga1993@gmail.com>
2023-02-26 23:36:54 +0900

    add parameter

--- modules/run_experiment.py
+++ modules/run_experiment.py
@@ -6,15 +6,14 @@ from modules import utils
 from modules import models
 from modules import preprocess
 
-def main(BASE, external, params, use_umap=False):
+def main(BASE, external, params, use_umap=False, add_location=True):
     mbd = 'microbusiness_density'
 
     df_train, df_test, df_subm = utils.load_dataset(BASE)
-    df_all, df_census = utils.merge_dataset(df_train, df_test, BASE=external, unemploy=False, census=True, fix_pop=True, coord=True, add_location=True, use_umap=use_umap, outlier=True)
+    df_all, df_census = utils.merge_dataset(df_train, df_test, BASE=external, unemploy=False, census=True, fix_pop=True, coord=True, add_location=add_location, use_umap=use_umap, outlier=True)
 
     instance_validation = models.LgbmBaseline('validation', df_subm, df_all, df_census, start_all_dict=32, save_path=False, params=params)
     instance_validation.accum_validation(m_len=5)
 
     instalce_prediction = models.LgbmBaseline('submission', df_subm, df_all, df_census, start_all_dict=40, save_path=False, params=params)
     instalce_prediction.create_submission()
-
