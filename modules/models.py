69c572e814ab16b64749c1b5ad32bff32feaca08
Kazuaki Tokunaga <ktokunaga1993@gmail.com>
2023-03-14 11:40:59 +0900

    update

--- modules/models.py
+++ modules/models.py
@@ -435,7 +435,7 @@ class LgbmBaseline():
                 if trend_method == 'mix':
                     idx = (~df_valid['mbd_short_trend'].isna())&(~df_valid['mbd_trend'].isna())
                     df_valid.loc[idx, 'mbd_trend'] = df_valid.loc[idx, 'mbd_trend'] * 0.5 + df_valid.loc[idx, 'mbd_short_trend'] * 0.5
-                    df_valid.loc[idx, 'mbd_pred'] = df_valid.loc[idx, 'mbd_trend'] * 0.7 + df_valid.loc[idx, 'mbd_model'] * 0.3
+                    df_valid.loc[idx, 'mbd_pred'] = df_valid.loc[idx, 'mbd_trend'] * 0.75 + df_valid.loc[idx, 'mbd_model'] * 0.25
                     print(f'# of cfips that have both short and long trend :', sum(idx))
             
 
